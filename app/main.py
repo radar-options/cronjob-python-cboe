975e02d42190468736a5bc6f6e56b1a99c8bb8e4
Ernesto Martínez del Pino <ernestomar1997@hotmail.com>
2022-10-22 17:27:35 +0200

    cd run DO publish

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from decouple import config
 from pymongo import MongoClient
 import pandas as pd
@@ -7,6 +8,7 @@ from radar_mongodb import QueryManager
 
 
 def main():
+    now = dt.datetime.now(tz = dt.timezone.utc)
     query_manager = QueryManager(
         db=MongoClient(config("MONGODB_URI")).get_default_database(),
         cboe_collection_name=config("CBOE_COLLECTION_NAME"),
@@ -14,8 +16,18 @@ def main():
     )
     cboe_extractor = CBOEExtractor(cboe_host=config("CBOE_HOST"))
     df: pd.DataFrame = cboe_extractor.to_dataframe()
+    current_price: float = cboe_extractor.get_current_price()
     df.dropna(axis=0, inplace=True)
-    query_manager.cboe_queries.insert_many(documents=df.to_dict(orient="records"))
+    query_manager.cboe_queries.insert_many(
+        documents=df.to_dict(orient="records"), 
+        now = now
+    )
+    query_manager.spot_queries.insert_many(
+        document={
+            "current_price": current_price
+        },
+        now = now
+    )
     print("success")
 
 
